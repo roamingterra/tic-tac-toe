d2f672d82f95ecf8e59330a227500c8666af3c9d
roamingterra <danielballerini1@gmail.com>
2023-01-25 18:57:51 -0500

    Create factory function logic for players
    I also started expanding the JavaScript logic by working on the
    gameBoard module, but this needs some rethinking. Finally, png images of
    the x and o icons have been added and can be found in the player factory
    function logic. The idea is that when a block on the board is clicked,
    an onclick attribute will trigger a function that will retrieve the
    player's icon and insert it into the block.

--- script.js
+++ script.js
@@ -5,13 +5,34 @@
 //      Check for winner funciton
 
 // Player factory function
-// Player X object created from factory function
-// Player O object created from factory function
+const playerFactory = (name, icon) => {
+  const playerName = () => name;
+  const playerIcon = document.createElement("img");
+  playerIcon.src = icon;
+  let score = 0;
 
-// Gameboard module
+  return { playerName, playerIcon, score };
+};
+// Player X object created from factory function
+const playerX = playerFactory("X", "images/x-icon.png");
+// Player O object created from factory function
+const playerO = playerFactory("O", "images/o-icon.png");
+
+// Gameboard module (I WAS TRYING TO STORE ALL GAME BLOCKS IN THE ARRAY, BUT I REALIZED I'M NOT STORING OBJECTS. I HAVE TO RETHINK THIS)
+// const gameBoard = (() => {
+//     let gameBoard = []
+//     const board = document.querySelector(".game-board");
+//     for(let i=0; i < board.childNodes.length; i++){
+//         let child = board.childNodes[i];
+//         for(let j=0; j < child.childNodes.length; i++){
+
+//         }
+//     }
+//     const block1 = document.querySelector()
+// })();
 //      gameboard array
 
-// Add marker to board function
+// Add marker to board function, onclick attribute function that places icon in chosen spot
 
 // Check for winner function
 
