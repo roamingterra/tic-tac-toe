3367fff040f5454b0af11806e5df66fba73b855a
roamingterra <danielballerini1@gmail.com>
2023-01-25 16:37:44 -0500

    Add some pseudocode in JavaScript
    I layed out some of the logic to kick off this JavaScript

---
+++ script.js
@@ -0,0 +1,18 @@
+// Control flow of game object (module?)
+//      Player X turn
+//      Check for winner function
+//      Player Y turn
+//      Check for winner funciton
+
+// Player factory function
+// Player X object created from factory function
+// Player O object created from factory function
+
+// Gameboard module
+//      gameboard array
+
+// Add marker to board function
+
+// Check for winner function
+
+// Restart game function
