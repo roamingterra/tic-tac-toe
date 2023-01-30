0044dd95000f19e055738d2bdee897fb967603a3
roamingterra <danielballerini1@gmail.com>
2023-01-30 17:43:26 -0500

    Write logic for game over and reset button
    I wrote multiple methods, that are stored in the proper modules and
    objects, that describe the logic for a game over and the reset button.
    The winning screens of each player, as well as the draw screen, are
    written in the html and styled in the CSS, so that they can be turned on
    and off easily through the various methods. Additionally, I fixed a bug
    that allowed a player to click an occupied box, which changed turns
    without a marker being placed. This was done by adding another condition
    to the block event listener.

--- script.js
+++ script.js
@@ -4,10 +4,12 @@ let players = [];
 
 // Player factory function
 const playerFactory = (playerName, icon, turn) => {
-  getPlayerName = () => playerName; // get functions accesses the value of the property
+  const getPlayerName = () => playerName; // get functions accesses the value of the property
   const getPlayerIcon = () => icon; // Also, arrow functions automatically return a value when there is one statement
   let getMyTurn = () => turn;
   let score = 0;
+  const setScore = () => score++;
+  const getScore = () => score;
 
   let changeTurns = () => {
     // And this would be a setter function, setting the value of the property
@@ -18,12 +20,19 @@ const playerFactory = (playerName, icon, turn) => {
     }
   };
 
+  const resetTurn = (resetTurn) => {
+    turn = resetTurn;
+  };
+
   return {
     // Note: JavaScript is always pass-by-value. This is why we need setters to change property values, and getters to access the values of those properties. Without the getter, the returned property value is always the original declared value
     getPlayerName,
     getPlayerIcon,
     getMyTurn,
+    setScore,
+    getScore,
     changeTurns,
+    resetTurn,
   };
 };
 
@@ -36,6 +45,10 @@ players.push(playerO);
 // Block factory function
 const blockFactory = (blockStatus, blockNumber) => {
   let blockMarker = document.createElement("img");
+
+  const getBlockStatus = () => blockStatus;
+
+  const resetBlockStatus = () => (blockStatus = "empty");
 
   let changeBlockStatus = () => {
     if (blockStatus === "empty") {
@@ -60,37 +73,50 @@ const blockFactory = (blockStatus, blockNumber) => {
     blockMarker.style.width = "30px";
   };
 
+  const removeMarker = () => {
+    blockMarker.removeAttribute("src", "");
+  };
+
   return {
+    getBlockStatus,
+    resetBlockStatus,
     changeBlockStatus,
     addMarker,
+    removeMarker,
   };
 };
 
-// Gameboard module to make block objects and store in gameBoardBlocks array
+// GameBoard module
 const gameBoard = (() => {
-  const board = document.querySelector(".game-board");
-  for (let i = 0; i < board.children.length; i++) {
-    let child = board.children[i];
-    for (let j = 0; j < child.children.length; j++) {
-      let block = blockFactory("empty", i * 3 + (j + 1));
-      gameBoardBlocks.push(block);
+  // Method that makes 9 block object and stores them in gameBoardBlocks array
+  const createBlockObjects = () => {
+    const board = document.querySelector(".game-board");
+    for (let i = 0; i < board.children.length; i++) {
+      let child = board.children[i];
+      for (let j = 0; j < child.children.length; j++) {
+        let block = blockFactory("empty", i * 3 + (j + 1));
+        gameBoardBlocks.push(block);
+      }
     }
-  }
-})();
+  };
 
-// Style page module to style the page dynamically
-const stylePage = (() => {
-  // Create function that highlights player when it is their turn
+  // Method that shows it is the appropriate players turn
   let highlightPlayer = () => {
     let playerXScore = document.querySelector(".x-score");
     let playerOScore = document.querySelector(".o-score");
+    let currentPlayerTurn = document.querySelector(".current-player-turn");
+
     if (playerX.getMyTurn() === true) {
+      currentPlayerTurn.textContent = "X Turn";
+
       playerXScore.style.borderBottomColor = "#22c55e";
       playerXScore.style.borderBottomWidth = "5px";
 
       playerOScore.style.borderBottomColor = "#737373";
       playerOScore.style.borderBottomWidth = "2px";
     } else if (playerO.getMyTurn() === true) {
+      currentPlayerTurn.textContent = "O Turn";
+
       playerXScore.style.borderBottomColor = "#737373";
       playerXScore.style.borderBottomWidth = "2px";
 
@@ -99,33 +125,244 @@ const stylePage = (() => {
     }
   };
 
-  return { highlightPlayer };
+  const checkWinner = () => {
+    if (
+      gameBoardBlocks[0].getBlockStatus() ===
+        gameBoardBlocks[1].getBlockStatus() &&
+      gameBoardBlocks[1].getBlockStatus() ===
+        gameBoardBlocks[2].getBlockStatus() &&
+      gameBoardBlocks[0].getBlockStatus() !== "empty" &&
+      gameBoardBlocks[1].getBlockStatus() !== "empty" &&
+      gameBoardBlocks[2].getBlockStatus() !== "empty"
+    ) {
+      return gameBoardBlocks[0].getBlockStatus();
+    } else if (
+      gameBoardBlocks[3].getBlockStatus() ===
+        gameBoardBlocks[4].getBlockStatus() &&
+      gameBoardBlocks[4].getBlockStatus() ===
+        gameBoardBlocks[5].getBlockStatus() &&
+      gameBoardBlocks[3].getBlockStatus() !== "empty" &&
+      gameBoardBlocks[4].getBlockStatus() !== "empty" &&
+      gameBoardBlocks[5].getBlockStatus() !== "empty"
+    ) {
+      return gameBoardBlocks[3].getBlockStatus();
+    } else if (
+      gameBoardBlocks[6].getBlockStatus() ===
+        gameBoardBlocks[7].getBlockStatus() &&
+      gameBoardBlocks[7].getBlockStatus() ===
+        gameBoardBlocks[8].getBlockStatus() &&
+      gameBoardBlocks[6].getBlockStatus() !== "empty" &&
+      gameBoardBlocks[7].getBlockStatus() !== "empty" &&
+      gameBoardBlocks[8].getBlockStatus() !== "empty"
+    ) {
+      return gameBoardBlocks[6].getBlockStatus();
+    } else if (
+      gameBoardBlocks[0].getBlockStatus() ===
+        gameBoardBlocks[3].getBlockStatus() &&
+      gameBoardBlocks[3].getBlockStatus() ===
+        gameBoardBlocks[6].getBlockStatus() &&
+      gameBoardBlocks[0].getBlockStatus() !== "empty" &&
+      gameBoardBlocks[3].getBlockStatus() !== "empty" &&
+      gameBoardBlocks[6].getBlockStatus() !== "empty"
+    ) {
+      return gameBoardBlocks[0].getBlockStatus();
+    } else if (
+      gameBoardBlocks[1].getBlockStatus() ===
+        gameBoardBlocks[4].getBlockStatus() &&
+      gameBoardBlocks[4].getBlockStatus() ===
+        gameBoardBlocks[7].getBlockStatus() &&
+      gameBoardBlocks[1].getBlockStatus() !== "empty" &&
+      gameBoardBlocks[4].getBlockStatus() !== "empty" &&
+      gameBoardBlocks[7].getBlockStatus() !== "empty"
+    ) {
+      return gameBoardBlocks[1].getBlockStatus();
+    } else if (
+      gameBoardBlocks[2].getBlockStatus() ===
+        gameBoardBlocks[5].getBlockStatus() &&
+      gameBoardBlocks[5].getBlockStatus() ===
+        gameBoardBlocks[8].getBlockStatus() &&
+      gameBoardBlocks[2].getBlockStatus() !== "empty" &&
+      gameBoardBlocks[5].getBlockStatus() !== "empty" &&
+      gameBoardBlocks[8].getBlockStatus() !== "empty"
+    ) {
+      return gameBoardBlocks[2].getBlockStatus();
+    } else if (
+      gameBoardBlocks[0].getBlockStatus() ===
+        gameBoardBlocks[4].getBlockStatus() &&
+      gameBoardBlocks[4].getBlockStatus() ===
+        gameBoardBlocks[8].getBlockStatus() &&
+      gameBoardBlocks[0].getBlockStatus() !== "empty" &&
+      gameBoardBlocks[4].getBlockStatus() !== "empty" &&
+      gameBoardBlocks[8].getBlockStatus() !== "empty"
+    ) {
+      return gameBoardBlocks[0].getBlockStatus();
+    } else if (
+      gameBoardBlocks[2].getBlockStatus() ===
+        gameBoardBlocks[4].getBlockStatus() &&
+      gameBoardBlocks[4].getBlockStatus() ===
+        gameBoardBlocks[6].getBlockStatus() &&
+      gameBoardBlocks[2].getBlockStatus() !== "empty" &&
+      gameBoardBlocks[4].getBlockStatus() !== "empty" &&
+      gameBoardBlocks[6].getBlockStatus() !== "empty"
+    ) {
+      return gameBoardBlocks[2].getBlockStatus();
+    }
+  };
+
+  const checkDraw = () => {
+    let isDraw = true;
+    for (let i = 0; i < gameBoardBlocks.length; i++) {
+      if (gameBoardBlocks[i].getBlockStatus() === "empty") {
+        isDraw = false;
+        return isDraw;
+      }
+    }
+    return isDraw;
+  };
+
+  const updateScore = (winner) => {
+    let winnerObjectInArray;
+    if (winner === players[0].getPlayerName()) {
+      winnerObjectInArray = 0;
+      players[0].setScore();
+    } else if (winner === players[1].getPlayerName()) {
+      winnerObjectInArray = 1;
+      players[1].setScore();
+    }
+
+    const winnerScore = document.querySelector(
+      "." + winner + "-score > span:nth-child(2)"
+    );
+
+    winnerScore.textContent = players[winnerObjectInArray].getScore();
+  };
+
+  const showWinner = (winner) => {
+    const gameBoard = document.querySelector(".game-board");
+    const xWinnerMessage = document.querySelector(".x-winner");
+    const oWinnerMessage = document.querySelector(".o-winner");
+    const currentPlayerTurn = document.querySelector(".current-player-turn");
+
+    currentPlayerTurn.textContent = "Game Over";
+    gameBoard.style.display = "none";
+
+    if (winner === "x") {
+      xWinnerMessage.style.display = "flex";
+    } else if (winner === "o") {
+      oWinnerMessage.style.display = "flex";
+    }
+  };
+
+  const showDraw = () => {
+    const gameBoard = document.querySelector(".game-board");
+    const drawMessage = document.querySelector(".draw");
+    const currentPlayerTurn = document.querySelector(".current-player-turn");
+
+    currentPlayerTurn.textContent = "Game Over";
+    gameBoard.style.display = "none";
+    drawMessage.style.display = "flex";
+  };
+
+  const showBoard = () => {
+    const gameBoard = document.querySelector(".game-board");
+    const xWinnerMessage = document.querySelector(".x-winner");
+    const oWinnerMessage = document.querySelector(".o-winner");
+    const drawMessage = document.querySelector(".draw");
+
+    gameBoard.style.display = "flex";
+    xWinnerMessage.style.display = "none";
+    oWinnerMessage.style.display = "none";
+    drawMessage.style.display = "none";
+  };
+
+  const resetBlockStatuses = () => {
+    for (let i = 0; i < gameBoardBlocks.length; i++) {
+      gameBoardBlocks[i].resetBlockStatus();
+    }
+  };
+
+  const removeMarkers = () => {
+    for (let i = 0; i < gameBoardBlocks.length; i++) {
+      gameBoardBlocks[i].removeMarker();
+    }
+  };
+
+  const resetTurns = () => {
+    players[0].resetTurn(true);
+    players[1].resetTurn(false);
+  };
+
+  return {
+    createBlockObjects,
+    highlightPlayer,
+    checkWinner,
+    checkDraw,
+    updateScore,
+    showWinner,
+    showDraw,
+    showBoard,
+    resetBlockStatuses,
+    removeMarkers,
+    resetTurns,
+  };
 })();
 
 // gameFlow module to control the progress of the game
 const gameFlow = (() => {
-  let continueGame = true;
+  let winner = "";
   const blocks = document.querySelectorAll(".block");
+  const reset = document.querySelector("#reset");
 
-  // Highlight player who's turn it is at the beginning of the game
-  stylePage.highlightPlayer();
+  // Create block objects
+  gameBoard.createBlockObjects();
+
+  // Show who's turn it is at the beginning of the game
+  gameBoard.highlightPlayer();
 
   blocks.forEach((block) => {
     block.addEventListener("click", (event) => {
       const blockNumber = event.target.id;
-      // Check to see if element clicked is a block and not a newly created player marker
-      if (blockNumber) {
+      // Check to see if element clicked is an empty block and not a newly created player marker or a filled block
+      if (
+        blockNumber &&
+        gameBoardBlocks[blockNumber - 1].getBlockStatus() === "empty"
+      ) {
         // Change block status
         gameBoardBlocks[blockNumber - 1].changeBlockStatus();
         // Change turn status for all players
         players[0].changeTurns();
         players[1].changeTurns();
-        // Change highlighted player after turns switch
-        stylePage.highlightPlayer();
         // Add player marker to block
         gameBoardBlocks[blockNumber - 1].addMarker();
+
+        // Check for winner
+        if (gameBoard.checkWinner()) {
+          winner = gameBoard.checkWinner();
+          gameBoard.updateScore(winner);
+          gameBoard.showWinner(winner);
+          winner = "";
+          return 0;
+        }
+
+        // Check for draw
+        if (gameBoard.checkDraw()) {
+          gameBoard.showDraw();
+          return 0;
+        }
+
+        // Change highlighted player after turns switch
+        gameBoard.highlightPlayer();
       }
     });
   });
-  //      Check for winner function
+
+  reset.addEventListener("click", () => {
+    gameBoard.resetBlockStatuses();
+    gameBoard.removeMarkers();
+    gameBoard.showBoard();
+    gameBoard.resetTurns();
+
+    // Reset highlighted players
+    gameBoard.highlightPlayer();
+  });
 })();
